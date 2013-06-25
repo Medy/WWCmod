1923bd0d3786663aa30ea966e2b1b63a9f6c270f
Medy <meidelyne@gmail.com>
2013-06-25 13:12:28 -1000

    edit some lines

--- WorldGeneratorWWCMod.java
+++ WorldGeneratorWWCMod.java
@@ -1,12 +1,10 @@
 package WWCmod;
 
 import java.util.Random;
-
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.IChunkProvider;
-import cpw.mods.fml.common.IWorldGenerator;
 import net.minecraft.world.gen.feature.WorldGenMinable;
-
+import cpw.mods.fml.common.IWorldGenerator;
 
 public class WorldGeneratorWWCMod implements IWorldGenerator {
 
@@ -14,7 +12,18 @@ public class WorldGeneratorWWCMod implements IWorldGenerator {
 	public void generate(Random random, int chunkX, int chunkZ, World world,
 			IChunkProvider chunkGenerator, IChunkProvider chunkProvider) {
 		// TODO Auto-generated method stub
-
+		switch(world.provider.dimensionId){
+		//case -1: generateNether(world, random,chunkX*16,chunkZ*16);
+		case 0 : generateSurface(world, random,chunkX*16,chunkZ*16);
 	}
 
 }
+
+	private void generateSurface(World world, Random random, int BlockX, int BlockZ) {
+		for(int i =0; i<10;i++){
+		int Xcoord = BlockX + random.nextInt(16);
+		int Zcoord = BlockZ + random.nextInt(16);
+		int Ycoord = random.nextInt(16);
+
+		(new WorldGenMinable(WWCMod.copperore.blockID, 8)).generate(world, random, Xcoord, Ycoord, Zcoord);
+		}}}
